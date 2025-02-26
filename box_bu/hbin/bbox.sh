6251dbb73cff45c04fb6f73ea2bab2e27b3d66aa
Hannes Bichler <h.bichler@bichler.tech>
2025-02-26 17:23:27 +0100

    move private box backup to btech_smart_web

--- box_bu/hbin/bbox.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#/etc/init.d/thttpd stop &>/dev/null
-sleep 2
-/sbin/shutdown -r now
--- box_bu/hbin/opkg_remove-package.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-test -z "${1}" && exit 1
-opkg --force-depends --force-remove --force-removal-of-dependent-packages --autoremove remove "${1}"
